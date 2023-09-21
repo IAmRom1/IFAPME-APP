dad4ec0ca14addacdac700a39f86e19d63ec2c58
Pcauderlier <pierre.cauderlier@outlook.com>
2023-09-21 11:08:08 +0200

    Ajout d'un fichier TEST er de toutes les classes

---
+++ Dossier_Test/var_init.js
@@ -0,0 +1,5 @@
+import {Cours, Formation, Classe, Jour, Semaine, Prof} from "../index.js"
+// Ce fichier initialise toutes les classe afin de pouvoir tester les algorithme de tris
+
+let s1 = new Semaine()
+console.log(s1)

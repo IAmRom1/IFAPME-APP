a60e635932340f28c77f4cf5fee75b924522738a
Pcauderlier <pierre.cauderlier@outlook.com>
2023-09-28 16:23:01 +0200

    ajout init variables

--- Dossier_Test/var_init.js
+++ Dossier_Test/var_init.js
@@ -1,4 +1,79 @@
-import {Cours, Formation, Classe, Jour, Semaine, Prof} from "../index.js"
+import {Cours, Formation, Classe, Jour, Semaine, Prof, Horaire, Local} from "../index.js"
 // Ce fichier initialise toutes les classe afin de pouvoir tester les algorithme de tris
+let X75 = new Formation("Developpement Web Front End 1ere année");
+let X77 = new Formation("Reseau premiere année");
+let X78 = new Formation("Cybersecurité premiere année");
+let X75b = new Formation("DevWeb 2 eme année");
+let X77b = new Formation("Reseau 2 eme année");
+let X78b = new Formation('CyberSecurité 2 eme année');
+X75.cours.push(
+    new Cours("Atelier développement,base",60,1, "",""),
+    new Cours("Anglais", 40,1,"",""),
+    new Cours("Language de balisage", 64, 1, "",""),
+    new Cours("Language de programmation, base", 80,2),
+    new Cours("Language de Base de donée, base", 48,3,"",""),
+    new Cours("Metier de devloppeur, base", 24, 2, "",""),
+    new Cours("Workshop ", 24,3)
+    )
+X77.cours.push(
+    new Cours("Le reseau",60,1, "",""),
+    new Cours("Anglais", 40,1,"",""),
+    new Cours("les cable", 64, 1, "",""),
+    new Cours("Les router, base", 80,2),
+    new Cours("Les switch", 48,3,"",""),
+    new Cours("Le wify", 24, 2, "",""),
+    new Cours("Le lan ", 24,3)
+)
+X78.cours.push(
+    new Cours("Html",60,1, "",""),
+    new Cours("Anglais", 40,1,"",""),
+    new Cours("CSS", 64, 1, "",""),
+    new Cours("BDD", 80,2),
+    new Cours("Reseau base", 48,3,"",""),
+    new Cours("Cybersecu WEB", 24, 2, "",""),
+    new Cours("Cyber secu APP ", 24,3)
+    )
+X75b.cours.push(
+    new Cours("Atelier développement,2",60,1, "",""),
+    new Cours("Anglais 2", 40,1,"",""),
+    new Cours("Language de balisage 2", 64, 1, "",""),
+    new Cours("Language de programmation, 2", 80,2),
+    new Cours("Language de Base de donée, 2", 48,3,"",""),
+    new Cours("Metier de devloppeur, 2", 24, 2, "",""),
+    new Cours("Workshop 2 ", 24,3)
+    )
+X77b.cours.push(
+    new Cours("Le reseau 2",60,1, "",""),
+    new Cours("Anglais 2", 40,1,"",""),
+    new Cours("les cable 2", 64, 1, "",""),
+    new Cours("Les router, 2", 80,2),
+    new Cours("Les switch 2", 48,3,"",""),
+    new Cours("Le wify 2", 24, 2, "",""),
+    new Cours("Le lan 2", 24,3)
+)
+X78b.cours.push(
+    new Cours("Html",60,1, "",""),
+    new Cours("Anglais", 40,1,"",""),
+    new Cours("CSS", 64, 1, "",""),
+    new Cours("BDD", 80,2),
+    new Cours("Reseau base", 48,3,"",""),
+    new Cours("Cybersecu WEB", 24, 2, "",""),
+    new Cours("Cyber secu APP ", 24,3)
+    )
 
-let language_balisage = new Cours()
+let listeClasses = [
+    new Classe('Dev Web Jour 1', X75,'J',14),
+    new Classe('Dev web Jour 2', X75b, 'J', 7),
+    new Classe('Reseau 1', X77,'J',12),
+    new Classe('Reseau 2', X77b, 'J', 8),
+    new Classe('CyberSécu 1', X78,'J',10),
+    new Classe('CyberSécu 2', X78b, 'J', 5)
+]
+let Local1 = new Local(); // Locaux seulement dispo en journée
+let Local2 = new Local().removeDispo('soir');
+
+Local1.disponibilite.lundi['soir'] = false
+
+console.log(Local2.disponibilite)
+
+
--- index.js
+++ index.js
@@ -1,28 +1,32 @@
 // Comande pour retrouver la classe d'un objet : objt.constructor.name
 
 export class Cours {   // Exemple : Language de balisage
-    constructor (nom, nombre_heures,etat_cours, ordre_prio, mot_cle, description){
+    constructor (nom, nombre_heures, ordre_prio , mot_cle, description){
         this.nom = nom.toLowerCase();               //str  passé en lettre minuscule
         this.nombre_heures = +(nombre_heures);         //number
-        this.etat_cours = etat_cours;               // true = finis / false = pas encore finis
+        this.etat_cours = false;               // true = finis / false = pas encore finis
         this.ordre_prio = +(ordre_prio);               // number (1,2,3,4) tout les 1 passe avant tout les 2 ect ... / 0 si bas-les-couille
-        this. mot_cle = mot_cle.map((e)=>e.toLowerCase());        // liste de str qui reprennent des mot associés au cours afin de le retrouver plus facilement (passée en minuscule)
+        this. mot_cle = mot_cle;        // liste de str qui reprennent des mot associés au cours afin de le retrouver plus facilement (passée en minuscule)
         this.description = description;             // courte description du cours
         this.prof = []                              // liste contenant des objet de classe Prof qui represente tout les profs capable de donné le cours
+
     }
 }
 
 export class Formation {       //Exemple : X75 Dev Web
     constructor (nom){
-        this.nom = nom;                     // Str 
+        this.nom = nom;                                    // Str 
         this.cours = [];                     // Liste d'objet de classe Cours
+    }
+    ordre_formation(){  // liste dont les 3 premiers sotn les cours a donner (ce suprime au fur et a mesure que les cours avance)
+        
     }
 }
 
 export class Classe {
     constructor(nom_classe, nom_formation, JN, nombre_eleves){
         this.nom_classe = nom_classe;            // STR 
-        this.nom_formation = nom_formation;      // Objet de classe Formation
+        this.formation = nom_formation;      // Objet de classe Formation
         this.JN = JN;                            // Str Jour ou Nuit (potentielement switch en booleen)
         this.nombre_eleves = +(nombre_eleves)   // Number 
         this.mail_eleves = []                   // Liste contenant les email de chaque élève de la classe (vide par défaut)
@@ -39,12 +43,13 @@ export class Jour{
 
 export class Semaine{
     constructor(){
-        this.lundi = new Jour();
+        this.lundi = new Jour()
         this.mardi = new Jour();
         this.mercredi = new Jour();
         this.jeudi = new Jour();
         this.vendredi = new Jour();
         this.samedi= new Jour(true,false,false);        // Samedi, seul le matin est libre par défaut
+        this.date_début = new Date()
     }
 }
 
@@ -56,8 +61,25 @@ export class Prof  {
     }
 }
 
-export class Horraire{
+export class Local{
     constructor(){
-        
+        this.disponibilite = new Semaine()
+    }
+    removeDispo(quand){ // Quand = matin/apresmidi / soir psour retirer toute les dispo d'un local à un certain moment
+        // for (let jour of Object.keys(this.disponibilite)){
+        //     this.disponibilite[jour][quand] = false
+        // }
+
+        this.disponibilite.lundi[quand] = false;
+        this.disponibilite.mardi[quand] = false;
+        this.disponibilite.mercredi[quand] = false;
+        this.disponibilite.jeudi[quand] = false;
+        this.disponibilite.vendredi[quand] = false;     // Sa marche pas jsp pk !!!!!!!!!!-!-!-!-!-!-!-!-!
+    }
+}
+export class Horaire{
+    constructor(nom){
+        this.nom = nom;
+        this.horaire = []                               //liste de semaines
     }
 }

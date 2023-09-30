bfa68cd938a47922f1a5593d8eb3e97b84f51823
Romain <profurax@gmail.com>
2023-09-30 21:59:05 +0200

    test devlog

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,37 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Logo from "../components/Logo";
 import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate();
 
-  
-  const handleSubmit = (e)=>{
+  const handleSubmit = async (e) => {
     e.preventDefault();
-  }
+    console.log("test");
+
+    try {
+      const response = await axios.post("http://localhost/database_ifapme/login.php", {
+        email: email,
+        password: password,
+      });
+
+      if (response.data.success) {
+        setMessage(response.data.message);
+
+        navigate("/Tableau_de_bord");
+      } else {
+        setMessage(response.data.message);
+      }
+      console.log("err", response.data.error);
+    } catch (error) {
+      console.error("Erreur Axios :", error);
+    }
+  };
 
   return (
     <div className="Login">
@@ -15,17 +39,29 @@ const Login = () => {
       <div className="container">
         <h1>Connexion</h1>
         <form>
-          <input type="email" placeholder="Email" name="email" id="email" />
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            id="email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <input
             type="password"
-            placeholder="Mots de passe"
+            placeholder="Mot de passe"
             name="password"
             id="password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
           <input onClick={handleSubmit} type="submit" value="Se connecter" />
         </form>
+        <p>{message}</p>
         <NavLink to="/Forget_Passwords">
-          <p>Mots de passe oublié?</p>
+          <p>Mot de passe oublié ?</p>
         </NavLink>
       </div>
     </div>
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,53 +1,61 @@
-import React, { useState } from 'react';
-import Logo from '../components/Logo';
+import React, { useState } from "react";
+import Logo from "../components/Logo";
 
 const Register = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [emailError, setEmailError] = useState('');
-    const [passwordError, setPasswordError] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [phone, setPhone] = useState("");
+  const [phoneError, setPhoneError] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-        if (!emailPattern.test(email)) {
-            setEmailError("Adresse email invalide");
-        } else {
-            setEmailError("");
-        }
-        if (password.length < 9) {
-            setPasswordError("Le mot de passe doit avoir au moins 9 caractères");
-        } else {
-            setPasswordError("");
-        }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    const phonePattern = /^\+?[0-9\s]+$/;
+    if (!emailPattern.test(email)) {
+      setEmailError("Adresse email invalide");
+    } else {
+      setEmailError("");
     }
+    if (!phonePattern.test(phone)) {
+      setPhoneError("Numéro de téléphone invalide");
+    } else {
+      setPhoneError("");
+    }
+    if (password.length < 8) {
+      setPasswordError("Le mot de passe doit avoir au moins 8 caractères");
+    } else {
+      setPasswordError("");
+    }
+  };
 
-    return (
-        <div className='Register'>
-            <Logo />
-            <div className="container">
-                <h1>S'inscrire</h1>
-                <form>
-                    <input type="text" placeholder='Nom' name="name" id="name"/>
-                    <input type="text" placeholder='Prénom' name="firstname" id="firstname" />
-                    <input type="email" placeholder='E-mail' name="email"id="email" 
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    {emailError && <p className="error">{emailError}</p>}
-                    <input type="number" placeholder='Tél.' name="phone" id="phone"
-                        min={0}
-                    />
-                    <input type="password" placeholder='Mots de passe' name="password" id="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    {passwordError && <p className="error">{passwordError}</p>}
-                    <input onClick={handleSubmit} type="submit" value="S'inscrire" />
-                </form>
-            </div>
-        </div>
-    );
+  return (
+    <div className="Register">
+      <Logo />
+      <div className="container">
+        <h1>S'inscrire</h1>
+        <form>
+          <input type="text" placeholder="Nom" name="name" id="name" required />
+          <input type="text" placeholder="Prénom" name="firstname" id="firstname" required/>
+          <input type="email" placeholder="E-mail" name="email" id="email" required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}/>
+          {emailError && <p className="error">{emailError}</p>}
+          <input type="text" placeholder="Tél." name="phone" id="phone" maxLength={17} minLength={13} required
+            min={0}
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}/>
+          {phoneError && <p className="error">{phoneError}</p>}
+          <input type="password" placeholder="Mots de passe" name="password" id="password" required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}/>
+          {passwordError && <p className="error">{passwordError}</p>}
+          <input onClick={handleSubmit} type="submit" value="S'inscrire" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
